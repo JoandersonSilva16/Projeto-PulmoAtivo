bcf0ce7fbc5f371e1bc68d99f264f7e8e17c60f0
JoandersonSilva16 <jsa35@aluno.ifal.edu.br>
2025-09-08 13:38:39 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,103 +1,108 @@
 import 'package:flutter/material.dart';
+import 'consultas.dart';
+import 'medicamentos.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.home),
-          ),
-          title: Text('Bem vindo(a), usuário!'),
-          centerTitle: true,
-          backgroundColor: Color(0xFF0b6a8f),
-          foregroundColor: Colors.white,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.people),
-            ),
-          ],
+      title: 'Navegação Consultas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.home),
         ),
-        backgroundColor: Color(0xFF41e0e6),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 100,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.only(left: 100, right: 100),
-                        backgroundColor: Color(0xFFd82424),
-                      ),
-                      child: Text('Medicamentos',
-                        style: TextStyle (
-                          color: Colors.white,
-                          fontSize: 17,
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                  ],
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.only(left: 130, right: 130),
-                    backgroundColor: Color(0xFF00bf63),
-                  ),
-                  child: Text('Horário',
-                    style: TextStyle (
-                      color: Colors.white,
-                      fontSize: 17,
-                    ),
-                  ),
-                ),
-
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.only(left: 130, right: 120),
-                      backgroundColor: Color(0xFF1f80ff)
-                  ),
-                  child: Text('Consultas',
-                    style: TextStyle (
-                      color: Colors.white,
-                      fontSize: 17,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.only(left: 130, right: 120),
-                      backgroundColor: Color(0xFF035752)
-                  ),
-                  child: Text('Ouvidoria',
-                    style: TextStyle (
-                      color: Colors.white,
-                      fontSize: 17,
-                    ),
-                  ),
-                ),
-              ],
+        title: Text('Bem vindo(a), usuário!'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF36b3b8),
+        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.network(
+              "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
             ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 100),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => Medicamentos()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  backgroundColor: Color(0xFFd82424),
+                ),
+                child: Text(
+                  'Medicamentos',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 130),
+                  backgroundColor: Color(0xFF00bf63),
+                ),
+                child: Text(
+                  'Histórico',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => Consultas()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 120),
+                  backgroundColor: Color(0xFF1f80ff),
+                ),
+                child: Text(
+                  'Consultas',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 130),
+                  backgroundColor: Color(0xFF035752),
+                ),
+                child: Text(
+                  'Ouvidoria',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+            ],
           ),
         ),
       ),
